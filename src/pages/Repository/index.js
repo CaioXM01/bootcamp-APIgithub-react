2c076496c0cc5806911155b6d3517e1989da3741
CaioXM01 <caioxm01@gmail.com>
2019-11-17 00:54:43 -0300

    Navegação e LocalStorage

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // import { Container } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default class Repository extends Component {
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    console.log(repoName);
+  }
+
+  render() {
+    return <h1>Repository</h1>;
+  }
 }
